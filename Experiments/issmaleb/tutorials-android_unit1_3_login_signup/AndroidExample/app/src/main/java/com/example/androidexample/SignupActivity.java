0d64ab062f27157d177706259033723c5fead42f
Issmale Bekri <issmale@iastate.edu>
2024-09-12 15:19:42 -0500

    experiment 3

---
+++ Experiments/issmaleb/tutorials-android_unit1_3_login_signup/AndroidExample/app/src/main/java/com/example/androidexample/SignupActivity.java
@@ -0,0 +1,78 @@
+package com.example.androidexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class SignupActivity extends AppCompatActivity {
+
+    private EditText usernameEditText;  // define username edittext variable
+    private EditText passwordEditText;  // define password edittext variable
+    private EditText confirmEditText;   // define confirm edittext variable
+    private EditText petNameEditText;
+    private Button loginButton;         // define login button variable
+    private Button signupButton;        // define signup button variable
+
+    private static final String PREFS_NAME = "MyPrefs";
+    private static final String KEY_PASSWORD = "password";
+    private static final String KEY_PET_NAME = "petName";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        /* initialize UI elements */
+        usernameEditText = findViewById(R.id.signup_username_edt);  // link to username edtext in the Signup activity XML
+        passwordEditText = findViewById(R.id.signup_password_edt);  // link to password edtext in the Signup activity XML
+        confirmEditText = findViewById(R.id.signup_confirm_edt);// link to confirm edtext in the Signup activity XML
+        petNameEditText = findViewById(R.id.signup_pet_name_edt);
+        loginButton = findViewById(R.id.signup_login_btn);    // link to login button in the Signup activity XML
+        signupButton = findViewById(R.id.signup_signup_btn);  // link to signup button in the Signup activity XML
+
+        /* click listener on login button pressed */
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                /* when login button is pressed, use intent to switch to Login Activity */
+                Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+                startActivity(intent);  // go to LoginActivity
+            }
+        });
+
+        /* click listener on signup button pressed */
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                /* grab strings from user inputs */
+                String username = usernameEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+                String confirm = confirmEditText.getText().toString();
+                String petName = petNameEditText.getText().toString();
+
+                if (password.equals(confirm)){
+                    // Save password and pet name to SharedPreferences
+                    SharedPreferences sharedPreferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putString(KEY_PASSWORD, password);
+                    editor.putString(KEY_PET_NAME, petName);
+                    editor.apply();
+                    Toast.makeText(getApplicationContext(), "Signing up", Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(SignupActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Password don't match", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+}
