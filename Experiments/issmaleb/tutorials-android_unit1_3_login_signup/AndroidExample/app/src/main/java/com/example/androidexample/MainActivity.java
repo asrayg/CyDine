0a02c151a7fecd91b3a5b92e3056e85117f59858
Issmale Bekri <issmale@iastate.edu>
2024-09-12 20:28:30 -0500

    settings and showing passwords and usernames

--- Experiments/issmaleb/tutorials-android_unit1_3_login_signup/AndroidExample/app/src/main/java/com/example/androidexample/MainActivity.java
+++ Experiments/issmaleb/tutorials-android_unit1_3_login_signup/AndroidExample/app/src/main/java/com/example/androidexample/MainActivity.java
@@ -1,15 +1,12 @@
 package com.example.androidexample;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     private Button loginButton;     // define login button variable
     private Button signupButton;    // define signup button variable
     private Button logoutButton;
+    private Button settingsButton;
 
     private static final String PREFS_NAME = "MyPrefs";
 
@@ -32,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
         loginButton = findViewById(R.id.main_login_btn);    // link to login button in the Main activity XML
         signupButton = findViewById(R.id.main_signup_btn);  // link to signup button in the Main activity XML
         logoutButton = findViewById(R.id.logout_btn);
+        settingsButton = findViewById(R.id.settings_btn);
+
 
         /* extract data passed into this activity from another activity */
         Bundle extras = getIntent().getExtras();
@@ -44,13 +44,13 @@ public class MainActivity extends AppCompatActivity {
             loginButton.setVisibility(View.INVISIBLE);              // set login button invisible
             signupButton.setVisibility(View.INVISIBLE);             // set signup button invisible
             logoutButton.setVisibility(View.VISIBLE);               // show logout button
+            settingsButton.setVisibility(View.VISIBLE);
         }
 
         /* click listener on login button pressed */
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 /* when login button is pressed, use intent to switch to Login Activity */
                 Intent intent = new Intent(MainActivity.this, LoginActivity.class);
                 startActivity(intent);
@@ -61,7 +61,6 @@ public class MainActivity extends AppCompatActivity {
         signupButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 /* when signup button is pressed, use intent to switch to Signup Activity */
                 Intent intent = new Intent(MainActivity.this, SignupActivity.class);
                 startActivity(intent);
@@ -83,6 +82,18 @@ public class MainActivity extends AppCompatActivity {
                 intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK); // Clear backstack
                 startActivity(intent);
                 finish();  // End the current MainActivity
+            }
+        });
+
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Pass the data through the intent to the SettingsActivity
+                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+                Bundle extras = getIntent().getExtras();
+                intent.putExtra("USERNAME", extras.getString("USERNAME"));
+                intent.putExtra("PASSWORD", extras.getString("PASSWORD"));  // Retrieve actual password securely
+                startActivity(intent);
             }
         });
     }
