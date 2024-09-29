5b5ecfac3f4f071d048a0fa045b588ba3d249360
arjun <arjun_patel1478@protonmail.com>
2024-09-29 17:29:15 -0500

    some backened boiler plate

---
+++ Backend/src/main/java/CyDine/Main.java
@@ -0,0 +1,44 @@
+package CyDine;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import CyDine.Users.User;
+import CyDine.Users.UserRepository;
+
+/**
+ * 
+ * @author Vivek Bengre
+ * 
+ */ 
+
+@SpringBootApplication
+class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+    // Create 3 users with their machines
+    /**
+     * 
+     * @param userRepository repository for the User entity
+     * Creates a commandLine runner to enter dummy data into the database
+     * As mentioned in User.java just associating the Laptop object with the User will save it into the database because of the CascadeType
+     */
+    @Bean
+    CommandLineRunner initUser(UserRepository userRepository) {
+        return args -> {
+            User user1 = new User("John", "john@somemail.com");
+            User user2 = new User("Jane", "jane@somemail.com");
+            User user3 = new User("Justin", "justin@somemail.com");
+            userRepository.save(user1);
+            userRepository.save(user2);
+            userRepository.save(user3);
+
+        };
+    }
+
+}
