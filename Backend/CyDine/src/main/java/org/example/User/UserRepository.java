ea3b52405cbb3db5134c3f3bf948f7e7c0eab21a
Akhil Pallem <akhilpallem@MacBook-Pro-27.local>
2024-09-29 14:49:20 -0500

    Updated

--- Backend/CyDine/src/main/java/org/example/User/UserRepository.java
+++ Backend/CyDine/src/main/java/org/example/User/UserRepository.java
@@ -1,4 +1,16 @@
 package org.example.User;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByUsername(String username);
+
+    User findByEmail(String email);
+
+    boolean existsByUsername(String username);
+
+    boolean existsByEmail(String email);
 }
