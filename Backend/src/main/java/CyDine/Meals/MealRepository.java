47156cac78a055be99729a6f30f565683e9a5fee
apallem <akhilpallem@cedar2-166.exnet.iastate.edu>
2024-10-05 15:54:04 -0500

    Tests

--- Backend/src/main/java/CyDine/Meals/MealRepository.java
+++ Backend/src/main/java/CyDine/Meals/MealRepository.java
@@ -2,13 +2,15 @@ package CyDine.Meals;
 
 import CyDine.Meals.MealPlans;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
-public interface MealRepository {
-    public interface UserRepository extends JpaRepository<MealPlans, Long> {
+public interface MealRepository extends JpaRepository<MealPlans, Long> {
 
-        MealPlans findById(int id);
+    MealPlans findById(int id);
 
-        void deleteById(int id);
+    void deleteById(int id);
 
-    }
+    List<MealPlans> findAll();
+
 }
+
