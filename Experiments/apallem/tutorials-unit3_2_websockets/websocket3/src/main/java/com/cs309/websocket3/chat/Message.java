1633cdd7866fa88c698365118be9eb108ccfee3b
Akhil Pallem <akhilpallem@MacBook-Pro-27.local>
2024-10-24 14:28:30 -0500

    Experiment changes

--- Experiments/apallem/tutorials-unit3_2_websockets/websocket3/src/main/java/com/cs309/websocket3/chat/Message.java
+++ Experiments/apallem/tutorials-unit3_2_websockets/websocket3/src/main/java/com/cs309/websocket3/chat/Message.java
@@ -1,7 +1,6 @@
 package com.cs309.websocket3.chat;
 
 import java.util.Date;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -11,14 +10,13 @@ import jakarta.persistence.Lob;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
-
 import lombok.Data;
 
 @Entity
 @Table(name = "messages")
 @Data
 public class Message {
-	@Id
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
@@ -31,46 +29,15 @@ public class Message {
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "sent")
     private Date sent = new Date();
-	
-	
-	public Message() {};
-	
-	public Message(String userName, String content) {
-		this.userName = userName;
-		this.content = content;
-	}
 
-    public Long getId() {
-        return id;
-    }
+    public Message() {}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
+    public Message(String userName, String content) {
         this.userName = userName;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
         this.content = content;
     }
 
-    public Date getSent() {
-        return sent;
+    public boolean canDelete(String username) {
+        return this.userName.equals(username);
     }
-
-    public void setSent(Date sent) {
-        this.sent = sent;
-    }
-
-    
 }
