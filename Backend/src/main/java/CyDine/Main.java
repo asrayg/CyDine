ae505e37d565d1fbd6f5c04582540d282488c7b4
Akhil Pallem <akhilpallem@MacBook-Pro-27.local>
2024-10-28 14:21:35 -0500

    Height and weight for user

--- Backend/src/main/java/CyDine/Main.java
+++ Backend/src/main/java/CyDine/Main.java
@@ -27,9 +27,9 @@ class Main {
     @Bean
     CommandLineRunner initUser(UserRepository userRepository, FoodItemsRepository foodItemsRepository) {
         return args -> {
-            User user1 = new User("John", "john@somemail.com", "Hey");
-            User user2 = new User("Jane", "jane@somemail.com", "Hey");
-            User user3 = new User("Justin", "justin@somemail.com", "Hey");
+            User user1 = new User("John", "john@somemail.com", "Hey", 1, 110);
+            User user2 = new User("Jane", "jane@somemail.com", "Hey", 2, 120);
+            User user3 = new User("Justin", "justin@somemail.com", "Hey", 3, 210);
             userRepository.save(user1);
             userRepository.save(user2);
             userRepository.save(user3);
