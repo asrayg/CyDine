6977cc04ba752d7a543423befe1c89dda9c41347
asrayg <asraygopa@gmail.com>
2024-10-31 13:59:36 -0500

    UI for Mod Screen

---
+++ Frontend/CyDine_Front/app/src/main/java/com/example/androidexample/MessageFeedAdapter.java
@@ -0,0 +1,46 @@
+package com.example.androidexample;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class MessageFeedAdapter extends RecyclerView.Adapter<MessageFeedAdapter.MessageViewHolder> {
+
+    private final List<String> messages;
+
+    public MessageFeedAdapter(List<String> messages) {
+        this.messages = messages;
+    }
+
+    @NonNull
+    @Override
+    public MessageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.message_feed_item, parent, false);
+        return new MessageViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MessageViewHolder holder, int position) {
+        holder.messageText.setText(messages.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return messages.size();
+    }
+
+    static class MessageViewHolder extends RecyclerView.ViewHolder {
+        TextView messageText;
+
+        public MessageViewHolder(@NonNull View itemView) {
+            super(itemView);
+            messageText = itemView.findViewById(R.id.messageText);
+        }
+    }
+}
