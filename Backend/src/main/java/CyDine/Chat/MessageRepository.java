aef398b71c64ac34c205e3a18e6e504b0738ade6
Akhil Pallem <akhilpallem@MacBook-Pro-27.local>
2024-11-01 12:15:35 -0500

    Chat for WebSocket Code

--- Backend/src/main/java/CyDine/Chat/MessageRepository.java
+++ Backend/src/main/java/CyDine/Chat/MessageRepository.java
@@ -1,7 +1,6 @@
 package CyDine.Chat;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import CyDine.Chat.Message;
 public interface MessageRepository extends JpaRepository<Message, Long>{
 
 
