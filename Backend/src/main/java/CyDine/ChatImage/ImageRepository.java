66c4d61f0da14c6822f659532bd3f05128bc0e72
arjun <arjun_patel1478@protonmail.com>
2024-11-05 04:16:23 -0600

    temporary

---
+++ Backend/src/main/java/CyDine/ChatImage/ImageRepository.java
@@ -0,0 +1,7 @@
+package CyDine.ChatImage;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImageRepository extends JpaRepository<Image, Long> {
+    Image findById(int id);
+}
